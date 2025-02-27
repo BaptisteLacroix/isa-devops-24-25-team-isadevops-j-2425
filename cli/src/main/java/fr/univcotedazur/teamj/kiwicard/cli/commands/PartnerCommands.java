5836489693d9671142d2acecc4d356181e16d010
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2025-02-27 15:21:05 +0100

    :white_check_mark: Add unit test for PartnerCommands #37

--- cli/src/main/java/fr/univcotedazur/teamj/kiwicard/cli/commands/PartnerCommands.java
+++ cli/src/main/java/fr/univcotedazur/teamj/kiwicard/cli/commands/PartnerCommands.java
@@ -1,6 +1,5 @@
-package fr.univcotedazur.teamj.kiwicard.cli.partner;
+package fr.univcotedazur.teamj.kiwicard.cli.commands;
 
-import fr.univcotedazur.teamj.kiwicard.cli.CliContext;
 import fr.univcotedazur.teamj.kiwicard.cli.model.CliItem;
 import fr.univcotedazur.teamj.kiwicard.cli.model.CliPartner;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +17,12 @@ public class PartnerCommands {
     private final WebClient webClient;
 
     @Autowired
-    public PartnerCommands(WebClient webClient, CliContext cliContext) {
+    public PartnerCommands(WebClient webClient) {
         this.webClient = webClient;
     }
 
-    @ShellMethod("Show all partners (seepartners)")
-    public String seepartners() {
+    @ShellMethod("Show all partners (partners)")
+    public String partners() {
         return webClient.get()
                 .uri(BASE_URI)
                 .retrieve()
@@ -33,8 +32,8 @@ public class PartnerCommands {
                 .block();
     }
 
-    @ShellMethod("Show items of a partner (seeitems PARTNER_ID)")
-    public String seeitems(long partnerId) {
+    @ShellMethod("Show items of a partner (partneritems PARTNER_ID)")
+    public String partneritems(long partnerId) {
         System.out.println("Récupération des items du partenaire "+partnerId + " : ");
         return webClient.get()
                 .uri(BASE_URI+"/"+partnerId+"/items")
