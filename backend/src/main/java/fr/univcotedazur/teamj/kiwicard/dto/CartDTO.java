8c8c4b77869d8fbe296948d4329fcaf3f84a8a4b
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2025-03-09 13:47:13 +0100

    refactor: removing unused IPerkDTO interface.#18

--- backend/src/main/java/fr/univcotedazur/teamj/kiwicard/dto/CartDTO.java
+++ backend/src/main/java/fr/univcotedazur/teamj/kiwicard/dto/CartDTO.java
@@ -1,18 +1,19 @@
 package fr.univcotedazur.teamj.kiwicard.dto;
 
 import fr.univcotedazur.teamj.kiwicard.entities.Cart;
+import fr.univcotedazur.teamj.kiwicard.entities.perks.AbstractPerk;
 
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public record CartDTO (long cartId, PartnerDTO partner, Set<CartItemDTO> items, List<IPerkDTO> perksList) {
+public record CartDTO (long cartId, PartnerDTO partner, Set<CartItemDTO> items, List<AbstractPerk> perksList) {
     public CartDTO(Cart entity) {
         this(
                 entity.getCartId(),
                 new PartnerDTO(entity.getPartner()),
                 entity.getItemList().stream().map(CartItemDTO::new).collect(Collectors.toSet()),
-                entity.getPerksToUse().stream().map(IPerkDTO::toDTO).toList()
+                entity.getPerksToUse()
         );
     }
 }
