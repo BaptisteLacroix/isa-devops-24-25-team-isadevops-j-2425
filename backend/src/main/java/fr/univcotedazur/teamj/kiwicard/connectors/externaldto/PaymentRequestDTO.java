d7b6bbd5ca681522a75c6ba2f3ad1e50a8eba753
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2025-03-13 16:43:33 +0100

    :ambulance: Fix PaymentRequestDTO fields #95

--- backend/src/main/java/fr/univcotedazur/teamj/kiwicard/connectors/externaldto/PaymentRequestDTO.java
+++ backend/src/main/java/fr/univcotedazur/teamj/kiwicard/connectors/externaldto/PaymentRequestDTO.java
@@ -1,6 +1,6 @@
 package fr.univcotedazur.teamj.kiwicard.connectors.externaldto;
 
 // External DTO (Data Transfert Object) to POST payment request to the external Bank system
-public record PaymentRequestDTO (String creditCard, double amount)
+public record PaymentRequestDTO (String cardNumber, double amount)
 {
 }
