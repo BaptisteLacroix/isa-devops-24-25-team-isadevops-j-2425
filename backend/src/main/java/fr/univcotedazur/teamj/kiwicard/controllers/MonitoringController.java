804534a13ceb780801ccfed745e539605175e681
Antoine Maïstre-Rice <antoinemaitre06@gmail.com>
2025-03-15 14:53:57 +0100

    ✨ add basic MonitoringController finding methods
    also add corresponding tests

--- backend/src/main/java/fr/univcotedazur/teamj/kiwicard/controllers/MonitoringController.java
+++ backend/src/main/java/fr/univcotedazur/teamj/kiwicard/controllers/MonitoringController.java
@@ -1,13 +1,12 @@
 package fr.univcotedazur.teamj.kiwicard.controllers;
 
-import fr.univcotedazur.teamj.kiwicard.components.PurchaseCatalog;
 import fr.univcotedazur.teamj.kiwicard.entities.Purchase;
+import fr.univcotedazur.teamj.kiwicard.exceptions.UnknownCustomerEmailException;
 import fr.univcotedazur.teamj.kiwicard.exceptions.UnknownPartnerIdException;
+import fr.univcotedazur.teamj.kiwicard.exceptions.UnknownPurchaseIdException;
+import fr.univcotedazur.teamj.kiwicard.interfaces.purchase.IPurchaseFinder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,15 +14,24 @@ import java.util.List;
 @RequestMapping("/monitoring")
 public class MonitoringController {
 
-    private PurchaseCatalog purchaseCatalog;
+    private final IPurchaseFinder purchaseCatalog;
 
-    @Autowired
-    public MonitoringController(PurchaseCatalog purchaseCatalog) {
+    public MonitoringController(IPurchaseFinder purchaseCatalog) {
         this.purchaseCatalog = purchaseCatalog;
     }
 
     @GetMapping("/partnerHistory/{partnerId}")
     public List<Purchase> partnerHistory(@PathVariable long partnerId) throws UnknownPartnerIdException {
         return purchaseCatalog.findPurchaseByPartnerId(partnerId);
+    }
+
+    @GetMapping("purchase/{purchaseId}")
+    public Purchase getPurchase(@PathVariable long purchaseId) throws UnknownPartnerIdException, UnknownPurchaseIdException {
+        return purchaseCatalog.findPurchaseById(purchaseId);
+    }
+
+    @GetMapping("purchase")
+    public List<Purchase> getByCustomerAndPartner(@RequestParam String customerEmail, @RequestParam long partnerId) throws UnknownCustomerEmailException, UnknownPartnerIdException {
+        return purchaseCatalog.findPurchasesByCustomerAndPartner(customerEmail, partnerId);
     }
 }
---
+++ backend/src/test/java/fr/univcotedazur/teamj/kiwicard/controllers/MonitoringControllerTest.java
@@ -0,0 +1,58 @@
+package fr.univcotedazur.teamj.kiwicard.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.univcotedazur.teamj.kiwicard.BaseUnitTest;
+import fr.univcotedazur.teamj.kiwicard.components.CustomerCatalog;
+import fr.univcotedazur.teamj.kiwicard.components.PurchaseCatalog;
+import fr.univcotedazur.teamj.kiwicard.dto.CustomerSubscribeDTO;
+import fr.univcotedazur.teamj.kiwicard.entities.Cart;
+import fr.univcotedazur.teamj.kiwicard.entities.Payment;
+import fr.univcotedazur.teamj.kiwicard.entities.Purchase;
+import fr.univcotedazur.teamj.kiwicard.exceptions.UnknownPartnerIdException;
+import fr.univcotedazur.teamj.kiwicard.interfaces.purchase.IPurchaseFinder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(MonitoringController.class)
+public class MonitoringControllerTest extends BaseUnitTest {
+
+    private final String BASE_URI = "/monitoring";
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    public IPurchaseFinder purchaseCatalog;
+
+    @BeforeEach
+    public void setup() {
+        var mockPurchase = new Purchase(mock(Payment.class), mock(Cart.class));
+        try {
+            when(purchaseCatalog.findPurchaseByPartnerId(1L)).thenReturn(List.of(mockPurchase));
+        } catch (UnknownPartnerIdException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void getPartnerHistoryIntegrationTest() throws Exception {
+        mockMvc.perform(get(BASE_URI + "/partnerHistory/" + 1L))
+                .andExpect(status().isOk());
+        verify(purchaseCatalog, times(1)).findPurchaseByPartnerId(1L);
+    }
+}
