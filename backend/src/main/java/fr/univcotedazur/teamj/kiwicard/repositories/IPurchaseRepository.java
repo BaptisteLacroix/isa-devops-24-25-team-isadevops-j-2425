1be040acde07ff7c466ca409e2fe296f625ff14a
Antoine Maïstre-Rice <antoinemaitre06@gmail.com>
2025-03-17 20:26:58 +0100

    💄 remove useless comment

--- backend/src/main/java/fr/univcotedazur/teamj/kiwicard/repositories/IPurchaseRepository.java
+++ backend/src/main/java/fr/univcotedazur/teamj/kiwicard/repositories/IPurchaseRepository.java
@@ -78,20 +78,3 @@ public interface IPurchaseRepository extends JpaRepository<Purchase, Long> {
             """
     )
     List<Purchase> findAllByPartner(long partnerId, int limit);
-
-//    @Query(
-//            """
-//                SELECT ci FROM Purchase p, Customer c
-//                join CartItem ci on ci member of p.cart.itemList
-//                where c.email = :customerEmail
-//                and p member of c.purchaseList
-//                AND p.cart.partner.partnerId = :partnerId
-//                ORDER BY p.payment.timestamp DESC
-//                limit :nbItems
-//            """)
-//    List<CartItem> findLastItemIdsByCustomerAndPartner(
-//            @Param("customerEmail") String customerEmail,
-//            @Param("partnerId") long partnerId,
-//            @Param("nbItemsConsumed") int nbItems);
-}
-
