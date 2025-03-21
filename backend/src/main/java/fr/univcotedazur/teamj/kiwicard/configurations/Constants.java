613a894db3f2caba0315cfcbbc2ac02417a40f94
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2025-03-21 11:49:41 +0100

    :recycle: move constant value in Constants.java #35

--- backend/src/main/java/fr/univcotedazur/teamj/kiwicard/configurations/Constants.java
+++ backend/src/main/java/fr/univcotedazur/teamj/kiwicard/configurations/Constants.java
@@ -1,6 +1,11 @@
 package fr.univcotedazur.teamj.kiwicard.configurations;
 
+import fr.univcotedazur.teamj.kiwicard.entities.Cart;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class Constants {
 
     private Constants() {
