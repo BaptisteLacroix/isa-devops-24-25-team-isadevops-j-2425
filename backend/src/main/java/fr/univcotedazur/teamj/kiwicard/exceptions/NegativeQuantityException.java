392c58b979a68babc5a038c5fb5fdbaba3fa1e4e
Firelods <clement.lefevre1@etu.unice.fr>
2025-04-02 19:22:02 +0200

    :bug: Fix sonar issues

--- backend/src/main/java/fr/univcotedazur/teamj/kiwicard/exceptions/NegativeQuantityException.java
+++ backend/src/main/java/fr/univcotedazur/teamj/kiwicard/exceptions/NegativeQuantityException.java
@@ -3,28 +3,15 @@ package fr.univcotedazur.teamj.kiwicard.exceptions;
 
 public class NegativeQuantityException extends Exception {
 
-    private String name;
-    private int potentialQuantity;
+    private final String name;
+    private final int potentialQuantity;
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-
     public int getPotentialQuantity() {
         return potentialQuantity;
-    }
-
-    public void setPotentialQuantity(int potentialQuantity) {
-        this.potentialQuantity = potentialQuantity;
-    }
-
-    public NegativeQuantityException() {
     }
 
     public NegativeQuantityException(String name, int potentialQuantity) {
